9faef5d31107a7eb7021b7238e5edee25436932f
Jin0331 <sempre813@naver.com>
2024-06-03 22:00:53 +0900

    Initial Commit

---
+++ Teams/TeamsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamsApp.swift
+//  Teams
+//
+//  Created by JinwooLee on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct TeamsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
