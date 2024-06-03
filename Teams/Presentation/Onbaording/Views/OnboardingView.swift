21ad61555a8d95f22afcccf222db42f7be83763f
Jin0331 <sempre813@naver.com>
2024-06-03 23:39:08 +0900

    [Design] Onbaording View UI 구성

--- Teams/Presentation/Onbaording/Views/OnboardingView.swift
+++ Teams/Presentation/Onbaording/Views/OnboardingView.swift
@@ -10,14 +10,32 @@ import SwiftUI
 struct OnboardingView: View {
     var body: some View {
         VStack {
-            Text("새싹톡을 사용하면 어디서나")
-                .font(.title)
-                .frame(maxWidth: .infinity, alignment: .center)
-            Text("팀을 모을 수 있습니다")
-                .font(.title)
-                .frame(maxWidth: .infinity, alignment: .center)
             
-            Image("")
+            VStack {
+                Text("새싹톡을 사용하면 어디서나")
+                    .title1()
+                    .frame(maxWidth: .infinity, alignment: .center)
+                Text("팀을 모을 수 있습니다")
+                    .title1()
+                    .frame(maxWidth: .infinity, alignment: .center)
+            }
+            .frame(height: 150)
+            
+            
+            Image(.onboarding)
+                .frame(width: 368, height: 400)
+            
+            Spacer()
+            
+            Button("시작하기") {
+                
+            }
+            .tint(.brandWhite)
+            .frame(width: 345, height: 44)
+            .title2()
+            .background(.brandGreen)
+            .cornerRadius(8)
+            .padding()
         }
     }
 }
