9357e409723e94a4437a75c42b8d852f52369aaa
Jin0331 <sempre813@naver.com>
2024-06-14 17:50:11 +0900

    [Add] Onboarding Coordinator 구성

--- Teams/Presentation/Onbaording/Coordinator/OnboardingCoordinator.swift
+++ Teams/Presentation/Onbaording/Coordinator/OnboardingCoordinator.swift
@@ -5,4 +5,46 @@
 //  Created by JinwooLee on 6/14/24.
 //
 
-import Foundation
+import ComposableArchitecture
+import SwiftUI
+import TCACoordinators
+
+@Reducer(state: .equatable)
+enum OnboardingScreen {
+    case onboarding(OnboardingFeature)
+    case auth(AuthFeature)
+    case signUp(SignUpFeature)
+    case emailLogin(EmailLoginFeature)
+}
+
+@Reducer
+struct OnboardingCoordinator {
+    @ObservableState
+    struct State : Equatable {
+        var routes : [Route<OnboardingScreen.State>]
+    }
+    
+    enum Action {
+      case router(IndexedRouterActionOf<OnboardingScreen>)
+    }
+}
+
+struct OnboardingCoordinatorView : View {
+    let store : StoreOf<OnboardingCoordinator>
+    
+    var body: some View {
+        TCARouter(store.scope(state: \.routes, action: \.router)) { screen in
+            switch screen.case {
+            case let .onboarding(store):
+                OnboardingView(store: store)
+            case let .auth(store):
+                AuthView(store: store)
+            case let .signUp(store : store):
+                SignUpView(store: store)
+            case let .emailLogin(store):
+                EmailLoginView(store: store)
+            }
+        }
+    }
+}
+
