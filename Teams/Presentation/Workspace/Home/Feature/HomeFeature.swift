911cd602c63787c0251bde26daf76d9ca6b13c9a
Jin0331 <sempre813@naver.com>
2024-07-06 01:22:34 +0900

    [fix] Workspace - onAppear 시점에 Channel, DM List Network 요청 수정

--- Teams/Presentation/Workspace/Home/Feature/HomeFeature.swift
+++ Teams/Presentation/Workspace/Home/Feature/HomeFeature.swift
@@ -14,13 +14,18 @@ struct HomeFeature {
     struct State : Equatable {
         let id = UUID()
         var workspaceCurrent : Workspace?
+        var channelList : ChannelList = []
+        var dmList : DMList = []
     }
 
     enum Action {
         case onAppear
         case openSideMenu
         case closeSideMenu
-        case myChannelResponse(Result<[Channel], APIError>)
+        
+        case channeListlResponse(Result<[Channel], APIError>)
+        case dmListResponse(Result<[DM], APIError>)
+        
     }
     
     @Dependency(\.networkManager) var networkManager
@@ -30,17 +35,44 @@ struct HomeFeature {
             switch action {
             
             case .onAppear :
-                
                 guard let workspace = state.workspaceCurrent else { return .none }
                 
-                return .run { send in
-                    await send(.myChannelResponse(
-                        networkManager.getMyChannels(request: WorkspaceIDDTO(workspace_id: workspace.id))
-                    ))
-                }
+                print(UserDefaultManager.shared.accessToken, workspace.id)
                 
-            case let .myChannelResponse(response):
-                print(response)
+                return .merge([
+                    .run { send in
+                        await send(.channeListlResponse(networkManager.getMyChannels(request: WorkspaceIDDTO(workspace_id: workspace.id))))
+                    },
+                    .run { send in
+                        await send(.dmListResponse(networkManager.getDMList(request: WorkspaceIDDTO(workspace_id: workspace.id))))
+                    }
+                ])
+                
+            case let .channeListlResponse(.success(response)):
+                
+                state.channelList = response
+                
+                return .none
+                
+            case let .dmListResponse(.success(response)):
+                
+                print(response, "🌟")
+                
+                return .none
+
+                
+            case let .channeListlResponse(.failure(error)):
+                let errorType = APIError.networkErrorType(error: error.errorDescription)
+                print(errorType, error, "❗️ channeListlResponse error")
+                
+                return .none
+                
+                
+            case let .dmListResponse(.failure(error)):
+                let errorType = APIError.networkErrorType(error: error.errorDescription)
+                if case .E13 = errorType {
+                    state.dmList = []
+                }
                 
                 return .none
                 
