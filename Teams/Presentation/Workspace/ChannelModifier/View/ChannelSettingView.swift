7932e31f71590c46d08a30375140b1f7546940aa
Jin0331 <sempre813@naver.com>
2024-07-16 02:02:05 +0900

    [Feat] Workspace - ScrollView 적용

--- Teams/Presentation/Workspace/ChannelModifier/View/ChannelSettingView.swift
+++ Teams/Presentation/Workspace/ChannelModifier/View/ChannelSettingView.swift
@@ -14,61 +14,32 @@ struct ChannelSettingView: View {
     let store : StoreOf<ChannelSettingFeature>
     @State private var expanded: Bool = false
     
-    let columns = [
-      //추가 하면 할수록 화면에 보여지는 개수가 변함
-        GridItem(.flexible()),
-        GridItem(.flexible()),
-        GridItem(.flexible()),
-        GridItem(.flexible()),
-        GridItem(.flexible())
-    ]
-    
     var body: some View {
         WithPerceptionTracking {
             
             NavigationStack {
+                
                 Divider().background(.brandWhite)
                 
-                VStack {
-                    Text("# " + store.channelCurrent!.name)
-                        .title2()
-                        .frame(maxWidth: .infinity, maxHeight: 18, alignment: .leading)
-                        .padding(15)
-                    
-                    Text(store.channelCurrent!.description)
-                        .bodyRegular()
-                        .multilineTextAlignment(.leading)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.horizontal, 15)
-                    
-                    DisclosureGroup(isExpanded: $expanded) {
-                        LazyVGrid(columns: columns) {
-                            ForEach(store.channelCurrentMemebers!, id: \.id) { user in
-                                VStack {
-                                    KFImage.url(user.profileImageToUrl)
-                                        .requestModifier(AuthManager.kingfisherAuth())
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fill)
-                                        .frame(width: 44, height: 44)
-                                        .cornerRadius(8)
-                                    Text(user.nickname)
-                                        .bodyRegular()
-                                }
-                            }
-                        }
-                        
-                    } label: {
-                        Text("멤버 (\(store.channelCurrentMemebers!.count))")
+                ScrollView {
+                    VStack {
+                        Text("# " + store.channelCurrent!.name)
                             .title2()
-                            .foregroundColor(.brandBlack)
-                            .frame(height: 56)
+                            .frame(maxWidth: .infinity, maxHeight: 18, alignment: .leading)
+                            .padding(15)
+                        
+                        Text(store.channelCurrent!.description)
+                            .bodyRegular()
+                            .multilineTextAlignment(.leading)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.horizontal, 15)
+                        
+                        memberView()
+                        
+                        Spacer()
                     }
-                    .tint(.brandBlack)
-                    .padding(.horizontal, 15)
-                    
                 }
                 
-                Spacer()
             }
             .onAppear {
                 store.send(.onAppear)
@@ -88,5 +59,43 @@ struct ChannelSettingView: View {
                 }
             }
         }
+    }
+}
+
+extension ChannelSettingView {
+    fileprivate func memberView() -> some View {
+        let columns = [
+          //추가 하면 할수록 화면에 보여지는 개수가 변함
+            GridItem(.flexible()),
+            GridItem(.flexible()),
+            GridItem(.flexible()),
+            GridItem(.flexible()),
+            GridItem(.flexible())
+        ]
+        
+        return DisclosureGroup(isExpanded: $expanded) {
+            LazyVGrid(columns: columns) {
+                ForEach(store.channelCurrentMemebers!, id: \.id) { user in
+                    VStack {
+                        KFImage.url(user.profileImageToUrl)
+                            .requestModifier(AuthManager.kingfisherAuth())
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 44, height: 44)
+                            .cornerRadius(8)
+                        Text(user.nickname)
+                            .bodyRegular()
+                    }
+                }
+            }
+            
+        } label: {
+            Text("멤버 (\(store.channelCurrentMemebers!.count))")
+                .title2()
+                .foregroundColor(.brandBlack)
+                .frame(height: 56)
+        }
+        .tint(.brandBlack)
+        .padding(.horizontal, 15)
     }
 }
