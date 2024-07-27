6159bcc705b59774876d9df03a30663684755054
Jin0331 <sempre813@naver.com>
2024-07-27 17:35:27 +0900

    [Fix] Workspace - Workspace 수정 시, HomeView onAppear 동작하도록 수정

--- Teams/Presentation/Workspace/Coordinator/WorkspaceCoordinator.swift
+++ Teams/Presentation/Workspace/Coordinator/WorkspaceCoordinator.swift
@@ -148,13 +148,13 @@ struct WorkspaceCoordinator {
                 state.popupPresent = nil
                 state.sidemenuOpen = false
                 
-                return .concatenate([.send(.tab(.home(.router(.routeAction(id: .home, action: .home(.timerOff)))))), .send(.onAppear)])
                 
             case .homeEmpty(.router(.routeAction(_, action: .workspaceAdd(.createWorkspaceComplete)))), .sideMenu(.router(.routeAction(_, action: .workspaceAdd(.createWorkspaceComplete)))):
                 print("workspace add complete 🌟🌟🌟🌟")
                 UserDefaultManager.shared.removeCurrentWorkspace()
-                return .concatenate([.send(.tab(.home(.router(.routeAction(id: .home, action: .home(.timerOff)))))),
-                                      .send(.closeSideMenu)])
+                return .concatenate([ .send(.tab(.home(.router(.routeAction(id: .home, action: .home(.timerOff)))))),
+                                      .send(.closeSideMenu),
+                                      .send(.onAppear)])
                 
             case .homeEmpty(.router(.routeAction(_, action: .emptyView(.openSideMenu)))), .tab(.home(.router(.routeAction(_, action: .home(.openSideMenu))))):
                 if let workspaceCurrent = state.workspaceCurrent {
@@ -208,7 +208,14 @@ struct WorkspaceCoordinator {
                 print("workspace exit or remove complete 🔆")
                 UserDefaultManager.shared.removeCurrentWorkspace()
                 return .concatenate([ .send(.tab(.home(.router(.routeAction(id: .home, action: .home(.timerOff)))))),
-                                      .send(.closeSideMenu)])
+                                      .send(.closeSideMenu),
+                                      .send(.onAppear)])
+                
+            case .sideMenu(.router(.routeAction(id: .workspaceEdit, action: .workspaceEdit(.editWorkspaceComplete)))):
+                print("workspace edit complete 🔆")
+                UserDefaultManager.shared.removeCurrentWorkspace()
+                return .concatenate([ .send(.tab(.home(.router(.routeAction(id: .home, action: .home(.timerOff)))))),
+                                      .send(.onAppear)])
                 
             case let .workspaceExitResponse(.failure(error)) :
                 
